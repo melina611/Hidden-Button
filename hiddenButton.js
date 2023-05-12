1ff287cd1ece7a32e0665576fd85c75d74bbfd0f
melina611 <132465720+melina611@users.noreply.github.com>
2023-05-12 14:41:07 +0300

    Update hiddenButton.js

--- hiddenButton.js
+++ hiddenButton.js
@@ -1,7 +1,6 @@
 let winnOrLose
 function hiddenButton() {
     winnOrLose = Math.floor(Math.random() * 3)
-    console.log(document.getElementsByTagName("button").length)
     for (let button of document.getElementsByTagName("button")) {
         button.addEventListener("click", function (e) {
             if ("winnerOrLoser" + winnOrLose == button.id) {
