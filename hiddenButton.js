faeaf2901394740222d7182e2610e1d16324a4a0
melina611 <132465720+melina611@users.noreply.github.com>
2023-05-12 14:39:24 +0300

    Update hiddenButton.js

--- hiddenButton.js
+++ hiddenButton.js
@@ -1,34 +1,15 @@
 let winnOrLose
 function hiddenButton() {
     winnOrLose = Math.floor(Math.random() * 3)
-    document.getElementById("buttonGroup").addEventListener("click", function (e) {
-        if (winnOrLose == 0) {
-            if (e.target === document.getElementById("winnerOrLoser0")) {
-                document.getElementById("winnerOrLoser0").innerHTML = "winner"
-            } else if (e.target === document.getElementById("winnerOrLoser1")) {
-                document.getElementById("winnerOrLoser1").innerHTML = "loser"
+    console.log(document.getElementsByTagName("button").length)
+    for (let button of document.getElementsByTagName("button")) {
+        button.addEventListener("click", function (e) {
+            if ("winnerOrLoser" + winnOrLose == button.id) {
+                button.innerHTML = "castigator"
             } else {
-                document.getElementById("winnerOrLoser2").innerHTML = "loser"
+                button.innerHTML = "necastigator"
             }
-        }
-        if (winnOrLose == 1) {
-            if (e.target === document.getElementById("winnerOrLoser0")) {
-                document.getElementById("winnerOrLoser0").innerHTML = "loser"
-            } else if (e.target === document.getElementById("winnerOrLoser1")) {
-                document.getElementById("winnerOrLoser1").innerHTML = "winner"
-            } else {
-                document.getElementById("winnerOrLoser2").innerHTML = "loser"
-            }
-        }
-        if (winnOrLose == 2) {
-            if (e.target === document.getElementById("winnerOrLoser0")) {
-                document.getElementById("winnerOrLoser0").innerHTML = "loser"
-            } else if (e.target === document.getElementById("winnerOrLoser1")) {
-                document.getElementById("winnerOrLoser1").innerHTML = "loser"
-            } else {
-                document.getElementById("winnerOrLoser2").innerHTML = "winner"
-            }
-        }
-    })
+        })
+    }
 }
 window.onload = hiddenButton
